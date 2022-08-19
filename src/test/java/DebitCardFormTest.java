536ce513ed379586793e130f30b09bd73a52d5a0
Елена Мурашева <vacilika77@gmail.com>
2022-08-19 20:49:30 +0300

    Added webdriver manager

--- src/test/java/DebitCardFormTest.java
+++ src/test/java/DebitCardFormTest.java
@@ -9,19 +9,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DebitCardFormTest {
-    private WebDriver driver;
+    //private WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -34,24 +44,23 @@ class DebitCardFormTest {
     void shouldTestV1() {
         driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Николай Иванов");
+        elements.get(0).sendKeys("Николай Иванов-Сидоров");
         elements.get(1).sendKeys("+79270000001");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
     void shouldTestV2() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Николай Иванов");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000001");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анна-Мария Соловей-Морозова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+74529871251");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
-
 
 }
